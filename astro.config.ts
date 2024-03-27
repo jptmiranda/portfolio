fe962604e1f24b17e09882c5d973023dd44d1672
João Miranda <joao.t.m@proton.me>
2024-03-27 21:59:00 +0000

    seo tweaks

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import compress from "astro-compress";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jptmiranda.com/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     icon(),
+    compress(),
+    robotsTxt(),
+    sitemap(),
   ],
 });
