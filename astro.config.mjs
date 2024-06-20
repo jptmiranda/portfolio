d31d04e857ddea243902b628f673321f1e6966fe
João Miranda <joao.t.m@proton.me>
2024-06-20 09:04:54 +0100

    blog wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import paraglide from "@inlang/paraglide-astro";
@@ -6,6 +7,7 @@ import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://jptmiranda.com/",
   i18n: {
@@ -28,6 +30,10 @@ export default defineConfig({
     paraglide({
       project: "./project.inlang",
       outdir: "./src/paraglide",
+    }),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "tokyo-night" },
     }),
   ],
 });
