c23463ad95c45179d4384253ef3668b84d5cb78f
João Miranda <joao.t.m@proton.me>
2024-07-05 11:42:47 +0100

    remove language switching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import paraglide from "@inlang/paraglide-astro";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
@@ -27,13 +26,18 @@ export default defineConfig({
     compress(),
     robotsTxt(),
     sitemap(),
-    paraglide({
-      project: "./project.inlang",
-      outdir: "./src/paraglide",
-    }),
     mdx({
       syntaxHighlight: "shiki",
-      shikiConfig: { theme: "tokyo-night" },
+      shikiConfig: {
+        theme: "min-dark",
+        transformers: [
+          {
+            pre(node) {
+              delete node.properties.tabindex;
+            },
+          },
+        ],
+      },
     }),
   ],
 });
