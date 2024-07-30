7a8f649b3fce2b4b3aeea75bf97820ee93ef7be3
João Miranda <joao.t.m@proton.me>
2024-07-30 22:13:33 +0100

    astro studio cms test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,46 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-import icon from "astro-icon";
-import robotsTxt from "astro-robots-txt";
-import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
+import robotsTxt from 'astro-robots-txt';
+import { defineConfig } from 'astro/config';
+
+import webVitals from '@astrojs/web-vitals';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jptmiranda.com/",
-  i18n: {
-    locales: ["en", "pt"],
-    defaultLocale: "en",
-  },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon({
-      include: {
-        carbon: ["launch", "copy"],
-        twemoji: ["flag-united-states", "flag-portugal"],
-      },
-    }),
-    compress(),
-    robotsTxt(),
-    sitemap(),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "min-dark",
-        transformers: [
-          {
-            pre(node) {
-              delete node.properties.tabindex;
+    site: 'https://jptmiranda.com/',
+    i18n: {
+        locales: ['en', 'pt'],
+        defaultLocale: 'en',
+    },
+    integrations: [
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        icon({
+            include: {
+                carbon: ['launch', 'copy'],
+                twemoji: ['flag-united-states', 'flag-portugal'],
             },
-          },
-        ],
-      },
-    }),
-  ],
+        }),
+        compress(),
+        robotsTxt(),
+        sitemap(),
+        mdx({
+            syntaxHighlight: 'shiki',
+            shikiConfig: {
+                theme: 'min-dark',
+                transformers: [
+                    {
+                        pre(node) {
+                            delete node.properties.tabindex;
+                        },
+                    },
+                ],
+            },
+        }),
+        webVitals(),
+    ],
 });
