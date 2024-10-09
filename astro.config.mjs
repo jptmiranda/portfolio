40c5cbdd5443858ff0cd0eb58091b1a2eba6ba63
João Miranda <joao.t.m@proton.me>
2024-10-09 03:32:27 +0100

    removed excess deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,51 +1,47 @@
-import db from '@astrojs/db';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import webVitals from '@astrojs/web-vitals';
-import compress from 'astro-compress';
-import icon from 'astro-icon';
-import robotsTxt from 'astro-robots-txt';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jptmiranda.com/',
-    output: 'server',
-    adapter: cloudflare(),
-    i18n: {
-        locales: ['en'],
-        defaultLocale: 'en',
-    },
-    integrations: [
-        tailwind({
-            applyBaseStyles: false,
-        }),
-        icon({
-            include: {
-                carbon: ['launch', 'copy'],
-                twemoji: ['flag-united-states', 'flag-portugal'],
+  site: "https://jptmiranda.com/",
+  output: "server",
+  adapter: cloudflare(),
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon({
+      include: {
+        carbon: ["launch", "copy"],
+        twemoji: ["flag-united-states", "flag-portugal"],
+      },
+    }),
+    compress(),
+    robotsTxt(),
+    sitemap(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "min-dark",
+        transformers: [
+          {
+            pre(node) {
+              delete node.properties.tabindex;
             },
-        }),
-        compress(),
-        robotsTxt(),
-        sitemap(),
-        mdx({
-            syntaxHighlight: 'shiki',
-            shikiConfig: {
-                theme: 'min-dark',
-                transformers: [
-                    {
-                        pre(node) {
-                            delete node.properties.tabindex;
-                        },
-                    },
-                ],
-            },
-        }),
-        webVitals(),
-        db(),
-    ],
+          },
+        ],
+      },
+    }),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,7 @@
-/// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
 declare namespace App {
-    // eslint-disable-next-line  @typescript-eslint/no-empty-interface
-    export interface Locals {}
+  // eslint-disable-next-line  @typescript-eslint/no-empty-interface
+  export interface Locals {}
 }
