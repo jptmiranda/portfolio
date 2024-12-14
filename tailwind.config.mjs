6b119384c8e4746f76928f24e15bafdb47e59a78
João Miranda <joao.t.m@proton.me>
2024-12-14 20:36:48 +0000

    preload font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,24 @@
-import defaultTheme from "tailwindcss/defaultTheme";
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        foreground: "hsl(var(--color-foreground) / <alpha-value>)",
-        background: "hsl(var(--color-background) / <alpha-value>)",
-        primary: "hsl(var(--color-primary) / <alpha-value>)",
-        secondary: "hsl(var(--color-secondary) / <alpha-value>)",
-        accent: "hsl(var(--color-accent) / <alpha-value>)",
-      },
-      fontFamily: {
-        "cascadia-code": ["CascadiaCode", ...defaultTheme.fontFamily.mono],
-      },
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                foreground: 'hsl(var(--color-foreground) / <alpha-value>)',
+                background: 'hsl(var(--color-background) / <alpha-value>)',
+                primary: 'hsl(var(--color-primary) / <alpha-value>)',
+                secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
+                accent: 'hsl(var(--color-accent) / <alpha-value>)',
+            },
+            fontFamily: {
+                'cascadia-code': [
+                    'CascadiaCode',
+                    ...defaultTheme.fontFamily.mono,
+                ],
+            },
+        },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
+    plugins: [require('@tailwindcss/typography')],
 };
